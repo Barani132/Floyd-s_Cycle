0a028264a5a4248d74de0e1b7e9ff4cbca263795
Barani132 <barani.b2137@gmail.com>
2026-04-08 21:58:33 +0530

    add script to Detect Loop in Linked List (Floyd’s Cycle)

---
+++ loop.py
@@ -0,0 +1,14 @@
+#Detect Loop in Linked List (Floyd’s Cycle)
+
+def has_cycle(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
